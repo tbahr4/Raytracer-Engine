9da2884d476a296a8a8fbb043867b7bb8acd52cc
Taylor Bahr <tbahr00@gmail.com>
2025-12-20 16:31:29 -0600

    Finalize configuration

--- include/Engine.h
+++ include/Engine.h
@@ -25,12 +25,12 @@ namespace Engine {
 
 		//! Rendering
 		constexpr double FOV = 90;
-		constexpr int SCREEN_WIDTH = 1920*2;
-		constexpr int SCREEN_HEIGHT = 1080*2;
+		constexpr int SCREEN_WIDTH = 1920;
+		constexpr int SCREEN_HEIGHT = 1080;
 		constexpr int NUM_RENDER_THREADS = 16;
 		constexpr int MAX_RAY_DEPTH = 5; // Maximum number of ray bounces to render
 		constexpr int NUM_RAYS_PER_TASK = 1000; // Number of ray traces to be packaged per worker thread
-		constexpr int RESOLUTION_DOWN_SCALE = 1; // Scales down the ray count (e.g., NxN pixels per ray)
+		constexpr int RESOLUTION_DOWN_SCALE = 2; // Scales down the ray count (e.g., NxN pixels per ray)
 
 		//! Lighting
 		const Util::Vector3<double> FLOOR_COLOR = { 45,45,45 };
